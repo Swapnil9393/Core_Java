881b1f48b3bd2fe23bdf77e8c0a1d765b70c3138
Swapnil Vitthal Taware <146724558+Swapnil9393@users.noreply.github.com>
2024-01-14 17:07:08 +0530

    Add files via upload

---
+++ MyName.java
@@ -0,0 +1,16 @@
+package firstpackage;
+
+public class MyName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Swapnil Vitthal Taware");
+      System.out.println(5/2);
+      System.out.println(5%2);
+      System.out.println(5.0/2);
+      System.out.println(5.0%2);
+	}
+	
+	
+
+}

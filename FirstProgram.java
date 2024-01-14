aab669c2857770bfd3c014536f8866090f8c0384
Swapnil Vitthal Taware <146724558+Swapnil9393@users.noreply.github.com>
2024-01-14 15:07:02 +0530

    Add files via upload
    First Java Program

---
+++ FirstProgram.java
@@ -0,0 +1,11 @@
+package firstpackage;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub.
+		System.out.println("Hello I am Swapnil");
+
+	}
+
+}

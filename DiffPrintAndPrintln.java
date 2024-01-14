d8d3fde8c1cc0678c602b1b8dc2781bd9ff4ebc4
Swapnil Vitthal Taware <146724558+Swapnil9393@users.noreply.github.com>
2024-01-14 21:02:22 +0530

    Printf, Println and Print
    Differences between Print f Print ln Print

---
+++ DiffPrintAndPrintln.java
@@ -0,0 +1,15 @@
+package firstpackage;
+
+public class DiffPrintAndPrintln {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a=10; float b=5.6f;
+		System.out.printf("Addition="+(a+b));
+		System.out.printf("\na=%d\tb=%f",a,b);
+		System.out.println("\nHello");
+		System.out.print("HI");
+
+	}
+
+}

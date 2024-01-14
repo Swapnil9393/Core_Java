d97d16edb0a712672335cdd6bc0f378222cf825f
Swapnil Vitthal Taware <146724558+Swapnil9393@users.noreply.github.com>
2024-01-14 20:04:42 +0530

    Addition
    Addition of Two Number

---
+++ AdditionTwoNumber.java
@@ -0,0 +1,17 @@
+package firstpackage;
+
+import java.util.Scanner;
+
+public class AdditionTwoNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter Two Values= ");
+		int a=s.nextInt();
+		int b=s.nextInt();
+		System.out.println("Addition= "+(a+b));
+
+	}
+
+}

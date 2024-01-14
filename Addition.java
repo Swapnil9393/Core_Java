e6e82da969658723a90a6c1fb7c9220ed4c464a6
Swapnil Vitthal Taware <146724558+Swapnil9393@users.noreply.github.com>
2024-01-14 17:07:58 +0530

    Add files via upload
    Addition

---
+++ Addition.java
@@ -0,0 +1,12 @@
+package firstpackage;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Addition="+(78+8));
+        int a=8,b=6;
+        System.out.println("a="+a+"\tb="+b+"\nAddition= "+(a+b)+"\nSubstraction= "+(a-b));
+	}
+
+}
